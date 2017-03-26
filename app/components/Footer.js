a832c8278ec559fdb7168b2d4393fed7327fdd7c
8845musign <8845musign@gmail.com>
2017-03-26 19:20:46 +0900

    Add Footer

---
+++ app/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+
+import './Footer.scss';
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="app-footer">
+        awesome footer.
+      </footer>
+    );
+  }
+}
--- app/index.js
+++ app/index.js
@@ -2,10 +2,12 @@ import React, { PropTypes } from 'react';
 import { render } from 'react-dom';
 
 import List from './components/List';
+import Footer from './components/Footer'
 
 render(
   <div className="container mt-4">
     <List />
+    <Footer />
   </div>,
   document.getElementById('app')
 );
